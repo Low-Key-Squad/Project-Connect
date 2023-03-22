a7379149eea3e5a9003c2e5e144ac2b90fbb5a57
PiotrFedak <piotr.fedak@studenci.collegiumwitelona.pl>
2023-03-22 23:31:11 +0100

    TailWind start

--- src/App.js
+++ src/App.js
@@ -6,17 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+       <button className="bg-black text-white text-3xl px-4 pt-2 hover:bg-blue-600 rounded"> button</button> 
       </header>
     </div>
   );
