8442cfc995f1f33920e9194cb9680feb6f65d7bc
PiotrFedak <piotr.fedak@studenci.collegiumwitelona.pl>
2023-04-19 23:58:40 +0200

    user appearance (not finished)+ icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Home from './components/Home';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div  className="App">
         <Home />
+        <Footer />
+        
+      
+
     
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,38 +1,22 @@
 import React from 'react'
+
 function Home() {
   return (
     
     
-    <div class="full flex items-center justify-center flex-col">
-
-      <div class="max-w-sm round overflow-hidden bg-gray-100 p-5 rounded-lg mt-4 text-white dark:bg-gray-900">
-        <img
-          class="w-full"
-          src="https://v1.tailwindcss.com/img/card-top.jpg"
-          alt="Sunset in the mountains"
-        />
-        <div class="px-6 py-4">
-          <div class="text-gray-800 text-center dark:text-gray-200 font-bold text-xl mb-2">
-            Connect
-          </div>
-          <p class="text-gray-800 dark:text-gray-200">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Voluptatibus quia, nulla! Maiores et perferendis eaque,
-            exercitationem praesentium nihil.
-          </p>
+    <div class="card">
+        <div class="imgBx">
+            <img src="img.jpg"/>
         </div>
-        <div class="px-6 pt-4 pb-2">
-          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            #meet
-          </span>
-          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            #new
-          </span>
-          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            #people
-          </span>
+        <div class="content">
+            <div class="details">
+                <h2>Alice Smith<br/><span>Alicja podróże małe i duże</span></h2>
+                <div class="actionBtn">
+                    <button>Dismiss</button>
+                    <button>Love</button>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
   )
 }
