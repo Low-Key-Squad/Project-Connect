347db138f8f0f1a1d086af09db5b779069a808c9
PiotrFedak <piotr.fedak@studenci.collegiumwitelona.pl>
2023-05-01 01:30:20 +0200

    a little bit of Google Authentication

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -1,13 +1,31 @@
 import React, { createContext, useContext } from "react";
 import * as Google from "expo-google-app-auth";
 
-const AuthContext = createContext({})
+const AuthContext = createContext({});
+
+   const config =  { 
+    //androidClientId: '' //link z googleService
+    //iosClientId: //link z googleServices
+    scopes: ["profile", "email"],
+    permissions: ["public_profile", "email", "gender", "location"],
+};
+
 
 export const AuthProvider = ({ children }) => {
+const signInWithGoogle = async () => {
+    Google.loginAsync(config).then(async (logInResult) => {
+        if (logInResult.type === "success") {
+           }
+        });
+    };
+
+
     return (
         <AuthContext.Provider value={{
-            user: "User",
-        }}>
+            user: null,
+            signInWithGoogle,
+        }}
+        >
             {children}
         </AuthContext.Provider>
     );
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -3,9 +3,15 @@ import { View, Text } from 'react-native';
 import useAuth from "../hooks/useAuth";
 
 const LoginScreen = () => {
+    const { signInWithGoogle } = useAuth();
+
     return (
         <View>
             <Text>Login</Text>
+            <Button title="login" onPress={signInWithGoogle} />
         </View>
-    )
-}
+    );
+};
+
+
+export default LoginScreen;
