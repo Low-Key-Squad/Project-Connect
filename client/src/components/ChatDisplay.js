9186855d3de24efda716448198e0deb47d256b20
KacperWojdak <kacper.wojdak@interia.pl>
2023-05-09 00:14:43 +0200

    ChatContainer / Setting Up NodeJS Backend

---
+++ client/src/components/ChatDisplay.js
@@ -0,0 +1,14 @@
+import Chat from "./Chat"
+import ChatInput from "./ChatInput"
+import React from "react"
+
+const ChatDisplay = () => {
+    return (
+        <>
+        <Chat/>
+        <ChatInput/>
+        </>
+    )
+}
+
+export default ChatDisplay
